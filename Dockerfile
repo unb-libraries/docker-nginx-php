42de5499882957b9bfac09d22af53d8bb34f11d9
Jacob Sanford <jsanford@unb.ca>
2016-02-21 19:38:38 -0400

    Introduce development environment switch to pipe logs and errors to docker journal

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM unblibraries/nginx:alpine
 MAINTAINER Jacob Sanford <jsanford_at_unb.ca>
 
 ENV COMPOSER_PATH /usr/local/bin
+ENV PHP_FPM_ERROR_LOG ${APP_LOG_DIR}/${APP_HOSTNAME}.php.error.log
 
 RUN apk --update add php-fpm php-json php-zlib php-xml php-phar php-gd php-iconv php-mcrypt curl php-openssl && \
   rm -f /var/cache/apk/* && \
--- scripts/pre-init.d/20_php-fpm.sh
+++ scripts/pre-init.d/20_php-fpm.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ "$DEPLOY_ENV" = "dev" ]; then
+  sed -i "s|PHP_FPM_ERROR_LOG|/proc/self/fd/2|g" /etc/php/php-fpm.conf
+else
+  sed -i "s|PHP_FPM_ERROR_LOG|$PHP_FPM_ERROR_LOG|g" /etc/php/php-fpm.conf
+fi
+
 sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/php/php-fpm.conf
-sed -i "s|APP_LOG_DIR|$APP_LOG_DIR|g" /etc/php/php-fpm.conf
 php-fpm
