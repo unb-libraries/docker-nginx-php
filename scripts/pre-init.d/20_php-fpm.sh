9d092f5bb4f85748800003e1c24dad3f8b6c193c
Jacob Sanford <jsanford@unb.ca>
2016-05-19 16:13:05 -0300

    Updates to tidy up user/group deployment

--- scripts/pre-init.d/20_php-fpm.sh
+++ scripts/pre-init.d/20_php-fpm.sh
@@ -5,5 +5,7 @@ else
   sed -i "s|PHP_FPM_ERROR_LOG|$PHP_FPM_ERROR_LOG|g" /etc/php/php-fpm.conf
 fi
 
-sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/php/php-fpm.conf
+sed -i "s|NGINX_RUN_USER|$NGINX_RUN_USER|g" /etc/php/php-fpm.conf
+sed -i "s|NGINX_RUN_GROUP|$NGINX_RUN_GROUP|g" /etc/php/php-fpm.conf
+
 php-fpm
