5ee3b027ce9e9b393e405b76a93435b3c3848c0d
Jacob Sanford <jsanford@unb.ca>
2017-03-20 21:15:53 -0300

    Update php-fpm binary to 7.1

--- scripts/pre-init.d/20_php-fpm.sh
+++ scripts/pre-init.d/20_php-fpm.sh
@@ -3,4 +3,4 @@ sed -i "s|PHP_FPM_ERROR_LOG|$PHP_FPM_ERROR_LOG|g" /etc/php7/php-fpm.d/zz_app.con
 sed -i "s|NGINX_RUN_USER|$NGINX_RUN_USER|g" /etc/php7/php-fpm.d/zz_app.conf
 sed -i "s|NGINX_RUN_GROUP|$NGINX_RUN_GROUP|g" /etc/php7/php-fpm.d/zz_app.conf
 
-/usr/sbin/php-fpm7
+/usr/sbin/php-fpm7.1
